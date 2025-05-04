dcc0464d5b22e7f9fabbc6dbe970fe9a3e8b2b9c
Abdalla Mohammed <abdalla143079@gmail.com>
2025-05-04 18:47:15 +0100

    fixed some bugs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='HBV_Lab',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(include=['HBV_Lab', 'HBV_Lab.*']),
     install_requires=[
         'numpy',
