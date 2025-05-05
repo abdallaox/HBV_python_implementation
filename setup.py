032e908efdca71ae69cd11cfd385600dc1b4ead4
Abdalla Mohammed <abdalla143079@gmail.com>
2025-05-05 12:54:20 +0100

    experimenting with the playground

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name='HBV_Lab',
     version='0.2.1',
-    packages=find_packages(include=['HBV_python', 'HBV_python.*']),
+    packages=find_packages(include=['HBV_Lab', 'HBV_Lab.*']),
     install_requires=[
         'numpy',
         'pandas',
